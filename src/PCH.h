6044b869be48d7c36fccf83e4b3e3c4041dcbb47
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-01-18 08:09:02 +0530

    update to latest clib

--- src/PCH.h
+++ src/PCH.h
@@ -6,15 +6,17 @@
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
-#include <SimpleIni.h>
+#include <ClibUtil/numeric.hpp>
+#include <ClibUtil/simpleINI.hpp>
+#include <ClibUtil/string.hpp>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <xbyak/xbyak.h>
 
 #define DLLEXPORT __declspec(dllexport)
 
 namespace logger = SKSE::log;
-namespace string = SKSE::stl::string;
-namespace numeric = SKSE::stl::numeric;
+namespace string = clib_util::string;
+namespace numeric = clib_util::numeric;
 
 using namespace std::literals;
 
